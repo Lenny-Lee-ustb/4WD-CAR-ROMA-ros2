469665e39554b8277ef0d6414956a0c6bf287353
gcy0115 <gcy15256266892@163.com>
2023-10-18 15:03:52 +0800

    remote wheel controller with launch

---
+++ src/wheel_remote_py/launch/wheel_remote_launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+# 定义函数名称为：generate_launch_description
+def generate_launch_description():
+    # 创建Actions.Node对象li_node，标明李四所在位置
+    node1 = Node(
+        package="wheel_remote_py",
+        executable="wheel_publisher"
+        )
+    # 创建Actions.Node对象wang2_node，标明王二所在位置
+    node2 = Node(
+        package="wheel_remote_py",
+        executable="wheel_receiver"
+        )
+    # 创建LaunchDescription对象launch_description,用于描述launch文件
+    launch_description = LaunchDescription([node1])
+    # 返回让ROS2根据launch描述执行节点
+    return launch_description
